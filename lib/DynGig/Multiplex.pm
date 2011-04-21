25ad1263243005042703a8b6e492c8a7acc60751
刘刊 <kan@cpan.org>
2011-04-21 04:47:15 +0000

    Import of KAN/DynGig-Multiplex-0.01 from CPAN.
    gitpan-cpan-distribution: DynGig-Multiplex
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         KAN/DynGig-Multiplex-0.01.tar.gz
    gitpan-cpan-author:       KAN
    gitpan-cpan-maturity:     released

---
+++ lib/DynGig/Multiplex.pm
@@ -0,0 +1,48 @@
+=head1 NAME
+
+DynGig::Multiplex - A collection of multiplex modules
+
+=cut
+package DynGig::Multiplex;
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+our $VERSION = '0.01';
+
+=head1 MODULES
+
+=head2 DynGig::Multiplex::CMD 
+
+Multiplexed Fork Client
+
+=head2 DynGig::Multiplex::TCP 
+
+Multiplexed TCP Client
+
+=head2 DynGig::Multiplex::CLI::CMD 
+
+CLI for DynGig::Multiplex::CMD
+
+=head2 DynGig::Multiplex::CLI::TCP 
+
+CLI for DynGig::Multiplex::TCP
+
+=head1 AUTHOR
+
+Kan Liu
+
+=head1 COPYRIGHT and LICENSE
+
+Copyright (c) 2010. Kan Liu
+
+This program is free software; you may redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+1;
+
+__END__
